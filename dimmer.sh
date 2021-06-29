ea6af7790d170e0e01ef7b7128053b817049bac5
Gustominox <agstcmps.111@gmail.com>
2021-06-29 01:32:57 +0100

    add dimmer script, fixing fn brightness keys not working

---
+++ dimmer.sh
@@ -0,0 +1,20 @@
+#! /bin/bash  
+if [ "$1" = 10 ]; then
+  percent="1"
+else
+  percent="0.$1"
+fi
+
+if [ "$1" = night ]; then
+  xrandr --output DP-2 --brightness 76 --gamma 1.1:0.8:0.7 && xrandr --output HDMI-0 --brightness 76 --gamma 1.1:0.8:0.7
+#else
+ # xrandr --output DP-2 --brightness 100 --gamma 1.1:0.8:0.7 && xrandr --output HDMI-0 --brightness 100 --gamma 1.1:0.8:0.7
+#fi
+#xrandr --output HDMI-0 --brightness "$percent" && xrandr --output DP-2 --brightness "$percent"
+else
+if [ "$2" = night ]; then
+  xrandr --output DP-2 --brightness "$percent" --gamma 1.1:0.8:0.7 && xrandr --output HDMI-0 --brightness "$percent" --gamma 1.1:0.8:0.7
+else
+  xrandr --output DP-2 --brightness "$percent" --gamma 1.0:1.0:1.0 && xrandr --output HDMI-0 --brightness "$percent" --gamma 1.0:1.0:1.0
+fi
+fi
