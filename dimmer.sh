b94f7319c5e06088c52bca81fcc4a7d822951a65
Gustominox <agstcmps.111@gmail.com>
2021-06-29 01:36:07 +0100

    changing gamma values for dimm_night alias

--- dimmer.sh
+++ dimmer.sh
@@ -13,7 +13,7 @@ if [ "$1" = night ]; then
 #xrandr --output HDMI-0 --brightness "$percent" && xrandr --output DP-2 --brightness "$percent"
 else
 if [ "$2" = night ]; then
-  xrandr --output DP-2 --brightness "$percent" --gamma 1.1:0.8:0.7 && xrandr --output HDMI-0 --brightness "$percent" --gamma 1.1:0.8:0.7
+  xrandr --output DP-2 --brightness "$percent" --gamma 1.1:0.8:0.7 && xrandr --output HDMI-0 --brightness "$percent" --gamma 1.0:0.85:0.85
 else
   xrandr --output DP-2 --brightness "$percent" --gamma 1.0:1.0:1.0 && xrandr --output HDMI-0 --brightness "$percent" --gamma 1.0:1.0:1.0
 fi
