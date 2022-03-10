32d6a8596a5dd83e17ccb7a06064a0c3d271fa96
Gustominox <agstcmps.111@gmail.com>
2022-03-10 14:20:37 +0000

    new enviroment terminal size

--- dimmer.sh
+++ dimmer.sh
@@ -1,4 +1,7 @@
 #! /bin/bash  
+
+	MONITORS=$(xrandr | awk ' / connected/ {print $1} ') #| wc -l
+
 if [ "$1" = 10 ]; then
   percent="1"
 else
@@ -6,7 +9,10 @@ else
 fi
 
 if [ "$1" = night ]; then
-  xrandr --output DP-2 --brightness 76 --gamma 1.1:0.8:0.7 && xrandr --output HDMI-0 --brightness 76 --gamma 1.1:0.8:0.7
+  for MONITOR in $MONITORS 
+    do xrandr --output $MONITOR --brightness 0.76 --gamma 1.1:0.8:0.7 
+    done
+  #xrandr --output DP-2 --brightness 76 --gamma 1.1:0.8:0.7 && xrandr --output HDMI-0 --brightness 76 --gamma 1.1:0.8:0.7
 #else
  # xrandr --output DP-2 --brightness 100 --gamma 1.1:0.8:0.7 && xrandr --output HDMI-0 --brightness 100 --gamma 1.1:0.8:0.7
 #fi
@@ -15,6 +21,8 @@ else
 if [ "$2" = night ]; then
   xrandr --output DP-2 --brightness "$percent" --gamma 1.1:0.8:0.7 && xrandr --output HDMI-0 --brightness "$percent" --gamma 1.0:0.85:0.85
 else
-  xrandr --output DP-2 --brightness "$percent" --gamma 1.0:1.0:1.0 && xrandr --output HDMI-0 --brightness "$percent" --gamma 1.0:1.0:1.0
+  for MONITOR in $MONITORS 
+    do xrandr --output $MONITOR --brightness "$percent" --gamma 1.0:1.0:1.0 
+    done
 fi
 fi
