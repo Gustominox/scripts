7e7471e3112574d66de39c95d42130761db593c5
Gustominox <agstcmps.111@gmail.com>
2025-04-29 02:24:44 +0100

    feat: add multiple monitors for night mode

--- dimmer.sh
+++ dimmer.sh
@@ -1,6 +1,6 @@
-#! /bin/bash  
+#!/bin/bash  
 
-	MONITORS=$(xrandr | awk ' / connected/ {print $1} ') #| wc -l
+MONITORS=$(xrandr | awk ' / connected/ {print $1} ') #| wc -l
 
 if [ "$1" = 10 ]; then
   percent="1"
@@ -10,19 +10,24 @@ fi
 
 if [ "$1" = night ]; then
   for MONITOR in $MONITORS 
-    do xrandr --output $MONITOR --brightness 0.76 --gamma 1.1:0.8:0.7 
+    do
+      echo "Adjusting settings for monitor: $MONITOR"
+      xrandr --output $MONITOR --brightness 0.76 --gamma 1.1:0.8:0.7 
     done
-  #xrandr --output DP-2 --brightness 76 --gamma 1.1:0.8:0.7 && xrandr --output HDMI-0 --brightness 76 --gamma 1.1:0.8:0.7
-#else
- # xrandr --output DP-2 --brightness 100 --gamma 1.1:0.8:0.7 && xrandr --output HDMI-0 --brightness 100 --gamma 1.1:0.8:0.7
-#fi
-#xrandr --output HDMI-0 --brightness "$percent" && xrandr --output DP-2 --brightness "$percent"
 else
-if [ "$2" = night ]; then
-  xrandr --output DP-2 --brightness "$percent" --gamma 1.1:0.8:0.7 && xrandr --output HDMI-0 --brightness "$percent" --gamma 1.0:0.85:0.85
-else
+  if [ "$2" = night ]; then
+    # xrandr --output DP-2 --brightness "$percent" --gamma 1.1:0.8:0.7 && xrandr --output HDMI-0 --brightness "$percent" --gamma 1.0:0.85:0.85
   for MONITOR in $MONITORS 
-    do xrandr --output $MONITOR --brightness "$percent" --gamma 1.0:1.0:1.0 
+    do
+      echo "Adjusting settings for monitor: $MONITOR"
+      xrandr --output $MONITOR --brightness "$percent" --gamma 1.1:0.8:0.7
     done
-fi
+  
+  else
+    for MONITOR in $MONITORS 
+      do
+        echo "Adjusting settings for monitor: $MONITOR"
+        xrandr --output $MONITOR --brightness "$percent" --gamma 1.0:1.0:1.0 
+      done
+  fi
 fi
